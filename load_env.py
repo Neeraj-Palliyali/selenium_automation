c6f272f0509927ef70ae024e1936a4cbb23cb45d
Neeraj-Palliyali <palliyali.neeraj@gmail.com>
2023-02-05 00:03:46 +0000

    INACCESS complete

--- load_env.py
+++ load_env.py
@@ -1,9 +1,18 @@
 import os
-from decouple import config
+import configparser
 
-CHROME_PATH = os.path.join(os.path.dirname(__file__), "chrome_driver", "chromedriver.exe")
-USER_DATA_PATH = os.path.join(os.path.dirname(__file__), "user_data_selenium", "")
+config = configparser.ConfigParser()
+config.read(r'settings.ini')
 
-INACESS_URL = config('INACESS_URL')
-INACCESS_USERNAME = config('INACCESS_USERNAME')
-INACCESS_PASSWORD = config('INACCESS_PASSWORD')
+CHROME_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "chrome_driver",
+    "chromedriver.exe")
+USER_DATA_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "user_data_selenium", "")
+
+INACESS_URL = config['settings']['INACESS_URL']
+INACCESS_USERNAME = config['logins']['INACCESS_USERNAME']
+INACCESS_PASSWORD = config['logins']['INACCESS_PASSWORD']
+INACCESS_FARMS = config['farms']['INACCESS_PASSWORD_FARMS'].split(',')
--- login.py
+++ login.py
@@ -1,6 +1,4 @@
-import time
 import load_env as env
-
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -10,40 +8,82 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 
-def login_inaccess(login :WebElement ):
+
+def login_inaccess(login: WebElement):
     print("------------Login Required------------")
-    password :WebElement = driver.find_element(By.ID,'password')
-    submit_show :WebElement = driver.find_element(By.CLASS_NAME, 'btn')
+    password: WebElement = driver.find_element(By.ID, 'password')
+    submit_show: WebElement = driver.find_element(By.CLASS_NAME, 'btn')
 
     login.send_keys(env.INACCESS_USERNAME)
-    password.send_keys(env.INACCESS_PASSWORD)   
-    time.sleep(4)
+    password.send_keys(env.INACCESS_PASSWORD)
     submit_show.submit()
     print("------------Login Successfull------------")
 
 
-    
+def set_200_items_per_page(driver: WebDriver):
+    print('------------Starting dropdown------------')
+    items_per_page: WebElement = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.CLASS_NAME, 'uds-btn-icon-clear'))
+    )
+    driver.execute_script("arguments[0].click();", items_per_page)
+
+    dropdown_xpath = '''//*[@id="NG2_UPGRADE_26_0"]/div/div[2]/pagination/div/
+    div[1]/div[3]/fang-dropdown/div/div/div[2]/div/div[4]/p'''
+
+    item_200: WebElement = driver.find_element(By.XPATH, dropdown_xpath)
+    driver.execute_script("arguments[0].click();", item_200)
+
+    print('------------Dropdown Selected------------')
+
+
+def open_pages(driver: WebDriver):
+    print('------------Opening pages------------')
+    for plant in env.INACCESS_FARMS:
+        element: WebElement = driver.find_element(
+            By.XPATH, f"//*[contains(text(), '{plant}')]"
+            )
+
+        if element:
+            actions = ActionChains(driver)
+            WebDriverWait(driver, 20).until(EC.visibility_of_element_located((
+                By.XPATH, f"//*[contains(text(), '{plant}')]"
+                )))
+            clickable = element.find_element(By.XPATH, "./../../../..")
+            driver.execute_script("arguments[0].scrollIntoView();", clickable)
+            actions.key_down(Keys.CONTROL) \
+                .perform()
+            clickable.click()
+            actions.key_up(Keys.CONTROL).perform()
+
+    print('------------Pages Opened------------')
+
+
 if __name__ == '__main__':
     print("------------Starting up------------")
-    # chromeOptions  = webdriver.ChromeOptions()
-    # chromeOptions .add_argument(f'user-data-dir = /user_data_selenium') 
-
 
     chrome_options = Options()
-    chrome_options.add_argument(f"--user-data-dir={env.USER_DATA_PATH}") 
+    chrome_options.add_argument(f"--user-data-dir={env.USER_DATA_PATH}")
     chrome_options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options )
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options)
     driver.get(env.INACESS_URL)
 
     try:
-        login :WebElement = WebDriverWait(driver, 10).until(
+        login: WebElement = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "username"))
         )
         if login:
             login_inaccess(login)
+
     except Exception as e:
-        print("jerjer")
-        
-    
+        print(e)
+
+    set_200_items_per_page(driver=driver)
+    open_pages(driver=driver)
